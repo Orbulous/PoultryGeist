2e7c15deb5131b20a96b46645c577098f9c72efb
L1ghteM <jaydenlong25@gmail.com>
2023-03-21 10:22:33 -0700

    Yeah
    ae

--- Assets/GavinRAssets/Jayden Shit/Jayden Shit/GameManager.cs
+++ Assets/GavinRAssets/Jayden Shit/Jayden Shit/GameManager.cs
@@ -5,22 +5,26 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject player;
+    public GameObject summoning;
     public GameObject spawnPoint;
+    public bool spawn;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
+        summoning = GameObject.Find("Summoning");
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        spawn = false;
     }
 
     public void Respawn()
     {
         player.transform.position = spawnPoint.transform.position;
+        spawn = true;
     }
 }
